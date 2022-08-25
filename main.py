b72459ccf01dc538578fe87f292dad673c2fdab5
Addy Ngo <110417388+addyng@users.noreply.github.com>
2022-08-25 18:27:29 +0000

    WIP for enemy movement to center

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ import math
 # Set screen dimensions
 screen_length = 300
 screen_height = 300
-centerx = screen_length // 2
-centery = screen_height // 2
 dim_field = (screen_length, screen_height)
 screen = pygame.display.set_mode(dim_field)
+(centerx, centery) = screen.get_rect().center
 
 # imports from assets
 background = pygame.image.load(os.path.join("assets","Grey_full.png"))
@@ -22,8 +21,10 @@ class player:
         '''Create player rectangle and set it in the center'''
         self.sizex = 10
         self.sizey = 10
+        self.x = centerx - self.sizex // 2
+        self.y = centery - self.sizey // 2
         # keep player centered
-        self.rect = pygame.Rect((screen_length - self.sizex) // 2, (screen_height - self.sizey) // 2, self.sizex, self.sizey) 
+        self.rect = pygame.Rect(self.x, self.y, self.sizex, self.sizey) 
 
     
 # enemy class
@@ -32,19 +33,35 @@ class enemy:
         '''Creates enemy at a random'''
         self.sizex = 10
         self.sizey = 10
-        self.angle = random.random() * 7
+        self.angle = random.random() * (math.pi*2)
         # x distance and y distance -> diagonal/hypotenuse from center
         self.x = centerx + int(distance * math.cos(self.angle))
         self.y = centery + int(distance * math.sin(self.angle))
         self.rect = pygame.Rect(self.x, self.y, self.sizex, self.sizey)
         enemies.append(self)
 
-    def move(self, speed = 10):
-        self.distancex = self.x - centerx
-        self.distancey = self.y - centery
-        self.angle = math.arctan(self.distancey / self.distancex)
-        self.rect.move_ip()
-        
+    def move(self, speed = 3):
+        if self.rect.colliderect(player.rect):
+            return
+        self.distancex = self.x - player.x 
+        self.distancey = self.y - player.y 
+        self.angle = math.atan(self.distancex / self.distancey)
+        # if self.distancey >= centery:
+        #     self.angle = self.angle + math.pi
+        print(self.angle*180/math.pi)
+        self.movex = speed * math.sin(self.angle)
+        self.movey = speed * math.cos(self.angle)
+        if self.y > player.y:
+            self.movex = -self.movex
+            self.movey = -self.movey
+        self.x = self.x + self.movex
+        self.y = self.y + self.movey
+        self.rect.move_ip(self.movex, self.movey)
+
+
+# if enemy xpos > centerx, take distance - speed to bring it to center
+# if enemy xpos < centerx, take distance + speed to bring it to center
+
 # projectile class
 
 player = player()
@@ -65,6 +82,7 @@ while running:
             if event.key == pygame.K_q:
                 running = False
 
+    e1.move()
     screen.blit(background,(0,0))
     pygame.draw.rect(screen, (255, 0, 0), player.rect)
     pygame.draw.rect(screen, (250,20,20), e1.rect)
